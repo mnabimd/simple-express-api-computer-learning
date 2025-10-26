9d0ba840e32507b89dec950f1ef61c794f51251a
Mohammad Nabi <mnabimd@gmail.com>
2025-10-26 09:55:25 +0430

    first version

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+// DB
+let users = [
+   { id: 1, name: 'Alice', email: 'alice@example.com' },
+   { id: 2, name: 'Bob', email: 'bob@example.com' },
+   { id: 3, name: 'Ahmad', email: 'ahmad@example.com' }
+];
+
+// Routes, EndPoints, URL
+// 1. /api/users   2. /api/tasks
+
+function sendUsers(req, res) {
+   res.send(users);
+}
+
+app.get('/api/users', sendUsers);
+
+// function addUser(req, res) {
+//    // 1. userName, 2. emailAddress
+//    const username = req.body.username;
+//    const email = req.body.email;
+
+//    console.log(username, email);
+// }
+
+// app.post('/api/users', addUser);
+
+app.listen(PORT, () => {
+   console.log('Express APP is running in PORT 3000');
+});
